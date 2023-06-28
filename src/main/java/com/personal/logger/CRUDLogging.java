be4bc3bc9c2d4e1f2d5c123d4e32262adcb1a131
Yannick Pire <yannick.pire@gmail.com>
2023-06-28 22:42:25 +0200

    Minor upgrade & changed aop logging level

--- src/main/java/com/personal/logger/CRUDLogging.java
+++ src/main/java/com/personal/logger/CRUDLogging.java
@@ -22,40 +22,40 @@ public class CRUDLogging {
     //Retrieving data from the database
     @After(value = "execution(* com.personal.service.ContactService.findEveryContact(..)) *)")
     public void logHomePage() {
-        LOGGER.info("********************************************************************");
-        LOGGER.info("********************************************************************");
-        LOGGER.info("Homepage is reached, and all the data is retrieved from the Database");
-        LOGGER.info("********************************************************************");
-        LOGGER.info("********************************************************************");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("Homepage is reached, and all the data is retrieved from the Database");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("********************************************************************");
     }
 
     //Inserting data into the database
     @After(value = "execution(* com.personal.service.ContactService.saveContact(..)) *)")
     public void logRegisterFormReached() {
-        LOGGER.info("********************************************************************");
-        LOGGER.info("********************************************************************");
-        LOGGER.info("A new contact contact was added: ");
-        LOGGER.info("********************************************************************");
-        LOGGER.info("********************************************************************");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("A new contact contact was added: ");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("********************************************************************");
     }
 
     //Removing contact from the database
     @After(value = "execution(* com.personal.service.ContactService.removeContact(..)) *)")
     public void logRemoveContact() {
-        LOGGER.info("********************************************************************");
-        LOGGER.info("********************************************************************");
-        LOGGER.info("A contact was removed from the database");
-        LOGGER.info("********************************************************************");
-        LOGGER.info("********************************************************************");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("A contact was removed from the database");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("********************************************************************");
     }
 
     //Updating contact in the database
     @After(value = "execution(* com.personal.service.ContactService.updateContact(..)) *)")
     public void logUpdate() {
-        LOGGER.info("********************************************************************");
-        LOGGER.info("********************************************************************");
-        LOGGER.info("A contact in the database was updated");
-        LOGGER.info("********************************************************************");
-        LOGGER.info("********************************************************************");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("A contact in the database was updated");
+        LOGGER.debug("********************************************************************");
+        LOGGER.debug("********************************************************************");
     }
 }
